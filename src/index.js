5195aa6eae912d368a70d49ffac5922f1bbd92a6
Daniel Dias <danieldias111@hotmail.com>
2021-06-03 13:35:13 +0100

    Promise.all

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const options = {
 
 const entryPoint = {
 	classId : '01p0900000HIpun',
-	token:'00D09000002VRMG!ARsAQN7Az1vhs2r6q6PQmudHZAakLpV.pQsjuV4LYuji6h_AEYpQiNdSJEXnxiCU3BAOkOxJQVCEzbWI7URiIAV1EQ5Fonf8',
+	token:'00D09000002VRMG!ARsAQC0yazsn3fwmJBfXdSqCkvEib_N5M9Vzc91bQPHYjVqzH_5_WB..bfLZMjujpLtEKl0KhAZ5IFOaZc0LG9xlTEpHZy.d',
 	url:'https://easy-deploy-dev-ed.my.salesforce.com'
 }
 
