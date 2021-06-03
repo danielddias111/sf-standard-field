3989177582e9a53633a31891ae95721fe0d996cd
Daniel Dias <danieldias111@hotmail.com>
2021-06-03 15:40:47 +0100

    Returning nothing if no fields exist

--- examples/example.js
+++ examples/example.js
@@ -1,20 +1,20 @@
 const getStandardFields = require('../src/index');
 
 const options = {
-	fields: ['testPablo__c.Id', 'account.industry', 'account.name', 'LEAD.FIRSTNAME', 'LEAD.industry','Lead.NotARealField']
+	fields: ['testPablo__c.Id']
 }
 
 const entryPoint = {
-	classId : '01p3h00000FHIq5',
-	token:'00D3h000005XLUw!AQkAQPdMhTi3H94c4TYQHg1VSq_eU2dKw9pOrjXv0YuXN4tIeVEi_J0W8SPpg.1Ulr81IE1jHUQJMRypuNYgmmXNIOhntpxv',
-	url:'https://brave-raccoon-mm7crl-dev-ed.my.salesforce.com'
+	classId : '01p0900000HIpun',
+	token:'00D09000002VRMG!ARsAQC0yazsn3fwmJBfXdSqCkvEib_N5M9Vzc91bQPHYjVqzH_5_WB..bfLZMjujpLtEKl0KhAZ5IFOaZc0LG9xlTEpHZy.d',
+	url:'https://easy-deploy-dev-ed.my.salesforce.com'
 }
 
 async function runExample(){
 
     console.log('Checking standard fields...')
-    //let response = await getStandardFields(entryPoint, options);
-    let response = await getStandardFields(entryPoint); //options is "optional"
+    let response = await getStandardFields(entryPoint, options);
+    //let response = await getStandardFields(entryPoint); //options is "optional"
     console.log(response);
 }
 
