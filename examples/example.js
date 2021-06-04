856b0e5fd8a341bf9738b6018ddfa930b2ab188d
Daniel Dias <danieldias111@hotmail.com>
2021-06-04 08:42:00 +0100

    Another bug, validating right condition in SOQL

--- examples/example.js
+++ examples/example.js
@@ -1,4 +1,4 @@
-const getStandardFields = require('../src/index');
+const { getStandardFields }= require('../src/index');
 
 const options = {
 	fields: []
@@ -6,7 +6,7 @@ const options = {
 
 const entryPoint = {
 	classId : '01p0900000HIpun',
-	token:'00D09000002VRMG!ARsAQC0yazsn3fwmJBfXdSqCkvEib_N5M9Vzc91bQPHYjVqzH_5_WB..bfLZMjujpLtEKl0KhAZ5IFOaZc0LG9xlTEpHZy.d',
+	token:'00D09000002VRMG!ARsAQB1_l0hS3cDMNt5RuIA0a.gUKgw7erw9PpJWnD5fJMqh7F1g3D4L5S_lJkb5OmgxDXxkmBU0zCCMS9.4wd8Wubuv3wpB',
 	url:'https://easy-deploy-dev-ed.my.salesforce.com'
 }
 
