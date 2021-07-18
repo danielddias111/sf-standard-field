74dc517da96e9c3537e9240bc713eea827c4f16b
Daniel Dias <danieldias111@hotmail.com>
2021-07-18 13:51:33 +0100

    Small bugs in SOQL

--- examples/example.js
+++ examples/example.js
@@ -1,20 +1,24 @@
 const { getStandardFields }= require('../src/index.js');
 
 const options = {
-	fields: ['Field_to_Track__c.id']
+	fields: ['Field_to_Track__c.id', 'Object_to_Track__c.name','Object_to_Track__c.id','user.name','user.id']
 }
 
 const entryPoint = {
-	classId : '01p09000000JkNsAAK',
+	classId : '',
 	token:'00D09000002VRMG!ARsAQLYvrK8YxCmG3EJ2DtyB_s5Ho5roLFtiuZN5uBaHvTw0etHRR55jbSonZ.Q31HP_2bGrSpkiN.DJScb9bHggVfsYMa9X',
 	url:'https://easy-deploy-dev-ed.my.salesforce.com'
 }
 
-async function runExample(){
-    console.log('Checking standard fields...')
-    let response = await getStandardFields(entryPoint, options);
-    //let response = await getStandardFields(entryPoint); //options is "optional"
-    console.log(response);
-}
+let classes = ['01p0900000LfnXDAAZ','01p09000000JkNsAAK']
 
+async function runExample(){
+		for(let i=0; i< classes.length; i++){
+			console.log('Checking standard fields... ' + classes[i])
+			entryPoint.classId = classes[i]
+			let response = await getStandardFields(entryPoint, options);
+			console.log(response);
+		}
+}
+	
 runExample();
