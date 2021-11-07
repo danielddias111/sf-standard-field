83e1d2408f9d3dbd716dd6394a0dd411a33f113a
Daniel Dias <danieldias111@hotmail.com>
2021-11-07 12:23:43 +0000

    Examples looping each class

--- README.md
+++ README.md
@@ -6,21 +6,50 @@ The response returns a map of field => boolean eg `{ 'orderitem.id' => false, 'o
 
 ```Javascript
 const {getStandardFields} = require('sf-standard-field-dependency')
+const fetch 	= require('node-fetch');
+const { getStandardFields }= require('../src/index.js');
 
 const options = {
-	fields: ['orderitem.id', 'opportunity.name']
+	fields: ['Account.Industry','Account.name','Lead.Industry']
 }
+const token = '00D09000007z6rW!ARQAQPCbHOmZyQZBZLS0tuz8_410nSjfE0_Cw4blNOa2Yddv1_Oa9uWtEyNqvadfNr_fIZGtr1jwunNvflVEkMUNWFBi8GX'
+const url = 'https://dependencypt-dev-ed.my.salesforce.com'
 
-const entryPoint = {
-	classId : 'myClassId',
-	token:'myToken',
-	url:'https://myCustomURL.my.salesforce.com'
+let classes = ['01p0900000O1eD8AAJ','01p0900000O1eDDAAZ','01p0900000O1eDIAAZ','01p0900000O1eDNAAZ','01p0900000O1eQPAAZ','01p0900000O1eQUAAZ','01p0900000O1eI4AAJ','01p0900000O1ehAAAR','01p0900000O1ehFAAR']
+let entryPoint
+
+async function runExamples(){
+	for(let i =0; i<classes.length; i++){
+		let endpoint = `${url}/services/data/v52.0/tooling/sobjects/ApexClass/${classes[i]}`;
+		let options2 = {
+				headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${token}`
+					}
+		}    
+		let response = await fetch(endpoint,options2);
+		let json = await response.json();
+		if(json.Body && json.SymbolTable){
+			const {Body,SymbolTable} = json;
+			entryPoint = {
+				token,
+				url,
+				SymbolTable,
+				Body
+			}
+			console.log(`Checking industry field in class: ${json.Name}`)
+			let response = await getStandardFields(entryPoint, options);
+			if(response != 'Nothing to analyse!!' && response.get('Account.Industry') && !response.get('Account.name') && !response.get('Lead.Industry')){
+				console.log('All good!')
+			}
+			else {
+				console.log(`Something went wrong ${entryPoint.classId}`)
+			}
+		}
+		else {
+			console.log(`Error on class: ${classes[i]}`)
+		}
+	}
 }
-
-async function runExample(){
-    console.log('Checking standard fields...')
-    let response = await getStandardFields(entryPoint, options);
-    console.log(response);
-}
-
-runExample();
+	
+runExamples();
--- examples/example.js
+++ examples/example.js
@@ -1,26 +1,47 @@
+const fetch 	= require('node-fetch');
 const { getStandardFields }= require('../src/index.js');
 
 const options = {
 	fields: ['Account.Industry','Account.name','Lead.Industry']
 }
+const token = '00D09000007z6rW!ARQAQPCbHOmZyQZBZLS0tuz8_410nSjfE0_Cw4blNOa2Yddv1_Oa9uWtEyNqvadfNr_fIZGtr1jwunNvflVEkMUNWFBi8GXc'
+const url = 'https://dependencypt-dev-ed.my.salesforce.com'
 
 let classes = ['01p0900000O1eD8AAJ','01p0900000O1eDDAAZ','01p0900000O1eDIAAZ','01p0900000O1eDNAAZ','01p0900000O1eQPAAZ','01p0900000O1eQUAAZ','01p0900000O1eI4AAJ','01p0900000O1ehAAAR','01p0900000O1ehFAAR']
+let entryPoint
 
-let entryPoint = {
-	//classId : '',
-	token:'00D09000007z6rW!ARQAQDLyk.mv5G4XLoGkicebSUZTz1_32hQgi162GVtub4uKAupSfWf12.wTe2M3835lDNqtBi1SKwGQilDbeIlL9g0KCyPk',
-	url:'https://dependencypt-dev-ed.my.salesforce.com',
-	SymbolTable:JSON.parse('{"constructors":[],"externalReferences":[],"id":"SOQLOrder","innerClasses":[],"interfaces":[],"key":"SOQLOrder","methods":[{"annotations":[],"location":{"column":24,"line":2},"modifiers":["static","public"],"name":"test","parameters":[],"references":[],"returnType":"void","type":null}],"name":"SOQLOrder","namespace":null,"parentClass":"","properties":[],"tableDeclaration":{"annotations":[],"location":{"column":14,"line":1},"modifiers":["public"],"name":"SOQLOrder","references":[],"type":"SOQLOrder"},"variables":[{"annotations":[],"location":{"column":23,"line":3},"modifiers":[],"name":"accList","references":[],"type":"List<Account>"}]}'),
-	Body:'public class SOQLOrder {\n    public static void test(){\n        List<Account> accList = [select id from account order by industry];\n    }\n}'
-}
-
-
-
-async function runExample(){
-		console.log('Checking standard fields... ' )
-		//entryPoint.classId = classes[i]
-		let response = await getStandardFields(entryPoint, options);
-		console.log(response);
+async function runExamples(){
+	for(let i =0; i<classes.length; i++){
+		let endpoint = `${url}/services/data/v52.0/tooling/sobjects/ApexClass/${classes[i]}`;
+		let options2 = {
+				headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${token}`
+					}
+		}    
+		let response = await fetch(endpoint,options2);
+		let json = await response.json();
+		if(json.Body && json.SymbolTable){
+			const {Body,SymbolTable} = json;
+			entryPoint = {
+				token,
+				url,
+				SymbolTable,
+				Body
+			}
+			console.log(`Checking industry field in class: ${json.Name}`)
+			let response = await getStandardFields(entryPoint, options);
+			if(response != 'Nothing to analyse!!' && response.get('Account.Industry') && !response.get('Account.name') && !response.get('Lead.Industry')){
+				console.log('All good!')
+			}
+			else {
+				console.log(`Something went wrong ${entryPoint.classId}`)
+			}
+		}
+		else {
+			console.log(`Error on class: ${classes[i]}`)
+		}
+	}
 }
 	
-runExample();
+runExamples();
